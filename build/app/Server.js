7616cd2d18879db3af173bbbe043aaabae4ad2b9
Sam Brownstone <sbrownrock@gmail.com>
2020-05-25 12:24:54 +0100

    fix scripting bugs

--- build/app/Server.js
+++ build/app/Server.js
@@ -3,15 +3,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Server = void 0;
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 class Server {
     constructor(app) {
         this.app = app;
-        this.app.use(express.static(path.resolve("./" + "build/client")));
+        this.app.use(express.json());
+        this.app.use(cors());
+        if (process.env.NODE_ENV === 'production') {
+            this.app.use(express.static(path.resolve("./" + "build/client")));
+            this.app.get(/.*/, (req, res) => {
+                res.sendFile(path.resolve("./" + "build/client/build/index.html"));
+            });
+        }
         this.app.get("/api", (req, res) => {
             res.send("You have reached the API!");
-        });
-        this.app.get("*", (req, res) => {
-            res.sendFile(path.resolve("./" + "build/client/index.html"));
         });
     }
     start(port) {
--- build/index.js
+++ build/index.js
@@ -6,6 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Server_1 = require("./app/Server");
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
-const port = 8080;
+const port = process.env.PORT;
 const server = new Server_1.Server(app);
 server.start(port);
