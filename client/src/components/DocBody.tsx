76b1f574349207983f11b6b1c57d202c56239b0a
Sam Brownstone <sbrownrock@gmail.com>
2020-06-01 00:07:29 +0100

    start new doc editing page

---
+++ client/src/components/DocBody.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import '../styles/DocBody.css'
+
+interface DocBodyProps {
+    bodyText: string;
+    onChange(value: string): void;
+    onEnter(): void;
+}
+
+function DocBody(props: DocBodyProps) {
+
+    const keyPressed = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (event.key === "Enter") {
+            props.onEnter()
+        }
+    }
+
+    return (
+        <div className="DocBody">
+            <textarea
+                className="DocBody-textbox"
+                rows={10}
+                placeholder="start writing here..."
+                value={props.bodyText}
+                onChange={event => props.onChange(event.target.value)}
+                onKeyUp={keyPressed}
+            ></textarea>
+        </div>
+    )
+}
+
+export default DocBody
--- client/src/components/DocView.tsx
+++ client/src/components/DocView.tsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/DocView.css'
+import DocBody from './DocBody'
+import { stat } from 'fs'
 
 function DocView() {
+    const [state, setState] = useState(
+        {
+            title: '',
+            body: ['THIS IS PARA 1', 'THIS IS PARA 2']
+        }
+    )
+
+    const fullBody = state.body.map((paragraph, index) => {
+        return (
+            <DocBody
+                bodyText={state.body[index]}
+                onChange={
+                    value => setState(() => {
+                        state.body[index] = value
+                        return {
+                            ...state
+                        }
+                    })
+                }
+                onEnter={
+                    () => setState(() => {
+                        state.body.splice(index + 1, 0, '')
+                        return {
+                            ...state
+                        }
+                    })
+                }
+            />
+        )
+    })
+
     return (
         <div className="DocView">
-            <input className="DocView-title" type="text" placeholder="title" />
+            <input
+                className="DocView-title"
+                type="text"
+                placeholder="title"
+                value={state.title}
+                onChange={event => setState({
+                    title: event.target.value,
+                    body: state.body
+                })}
+            />
+            {fullBody}
         </div>
     )
 }
