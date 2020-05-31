244be9078de0197dbf26c167a686ba48fb72c932
Sam Brownstone <sbrownrock@gmail.com>
2020-05-31 18:49:52 +0100

    start FE work

---
+++ client/src/components/DocView.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/DocView.css'
+
+function DocView() {
+    return (
+        <div className="DocView">
+            <input className="DocView-title" type="text" placeholder="title" />
+        </div>
+    )
+}
+
+export default DocView
