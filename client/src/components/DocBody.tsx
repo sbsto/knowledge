a970972cfdc133e23479ff3a7fc607ac5e0feee3
Sam Brownstone <sbrownrock@gmail.com>
2020-06-03 14:34:59 +0100

    add backspace to delete para funcitonality

--- client/src/components/DocBody.tsx
+++ client/src/components/DocBody.tsx
@@ -5,6 +5,7 @@ interface DocBodyProps {
     bodyText: string;
     onChange(value: string): void;
     onEnter(): void;
+    onBackspace(): void;
 }
 
 function DocBody(props: DocBodyProps) {
@@ -13,17 +14,21 @@ function DocBody(props: DocBodyProps) {
         if (event.key === "Enter") {
             props.onEnter()
         }
+
+        if (event.key === "Backspace") {
+            props.onBackspace()
+        }
     }
 
     return (
         <div className="DocBody">
             <textarea
                 className="DocBody-textbox"
-                rows={10}
+                rows={2}
                 placeholder="start writing here..."
                 value={props.bodyText}
                 onChange={event => props.onChange(event.target.value)}
-                onKeyUp={keyPressed}
+                onKeyDown={keyPressed}
             ></textarea>
         </div>
     )
