915c613174b9a811708e4ceb06e5d3dc46ab44d7
AsherWright <sastreen@hotmail.com>
2020-06-12 19:07:46 -0300

    BAO NOT BAO

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,87 @@
 import React from 'react';
-import { Container, Typography } from '@material-ui/core'
-import { DocControl } from './components/'
+import { Container, Grid, Paper, Slider, Typography, makeStyles, TextField, FormGroup, Checkbox, FormControlLabel, Button } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  header: {
+    padding: theme.spacing(2),
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(30);
+  const [valueChicken, setValueChicken] = React.useState(false);
+  const [valueShrimp, setValueShrimp] = React.useState(false);
+  const [valueVeggie, setValueVeggie] = React.useState(false);
+  const [valueSubmitted, setValueSubmitted] = React.useState(false);
+
+  const handleSliderChange = (event: any, newValue: any) => {
+    setValue(newValue);
+  };
+
+  const handleChicken = () => {
+    valueChicken ? setValueChicken(false) : setValueChicken(true)
+  }
+
+  const handleShrimp = () => {
+    valueShrimp ? setValueShrimp(false) : setValueShrimp(true)
+  }
+
+  const handleVeggie = () => {
+    valueVeggie ? setValueVeggie(false) : setValueVeggie(true)
+  }
+
+  const handleValueSubmitted = () => {
+    setValueSubmitted(true)
+  }
+
+  const submitted = () => {
+    return valueSubmitted ? <Typography className={classes.header} variant="h6">Thanks for your submission!</Typography> : null
+  }
+
   return (
     <Container className="App" maxWidth="lg">
-      {/* Instead of the below, we will have a funcitonal header component.
-          This is just a placeholder. I think the material-ui "App Bar" component
-          could look really nice here.
-      */}
-      <Typography variant="h1">home</Typography>
-      <DocControl />
+      <div className={classes.root}>
+        <Typography className={classes.header} variant="h1">Bao not Bao</Typography>
+        <Typography className={classes.header} variant="h6">Welcome to the debut of Bao not Bao! It's great to see you.</Typography>
+        <Typography className={classes.header} variant="h6" id="continuous-slider" gutterBottom>Please input your current level of anticipation:</Typography>
+        <Grid className={classes.header} container spacing={3}>
+          <Grid item xs={12}>
+            <Slider value={value} onChange={handleSliderChange} aria-labelledby="continuous-slider" />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography className={classes.header} variant="h6">Guest Name:</Typography>
+            <TextField></TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography className={classes.header} variant="h6">Please select up to 2 (TWO) Bao options:</Typography>
+          </Grid>
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox checked={valueChicken} onChange={handleChicken} name="gilad" />}
+              label="Chicken"
+            />
+            <FormControlLabel
+              control={<Checkbox checked={valueShrimp} onChange={handleShrimp} name="jason" />}
+              label="Tofu"
+            />
+            <FormControlLabel
+              control={<Checkbox checked={valueVeggie} onChange={handleVeggie} name="antoine" />}
+              label="Veggie"
+            />
+          </FormGroup>
+        </Grid>
+        <Button variant="contained" color="primary" onClick={handleValueSubmitted}>Submit</Button>
+        {submitted()}
+      </div>
     </Container>
   );
 }
