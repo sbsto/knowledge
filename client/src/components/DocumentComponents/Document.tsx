d654045f2607673a02e6a5c8126ab2c596fd5ac7
Sam Brownstone <sbrownrock@gmail.com>
2020-09-14 20:46:53 +0100

    remove unnecessary code

--- client/src/components/DocumentComponents/Document.tsx
+++ client/src/components/DocumentComponents/Document.tsx
@@ -68,33 +68,6 @@ function Document() {
             <Grid item md={8}>
                 <Title value={content.title} degree={0} />
                 {body}
-                {/* <Section
-                    title="New Document"
-                    content={[
-                        {
-                            title: "Introduction",
-                            content: [
-                                { content: "But who can say what's best? That's why you need to grab whatever chance you have of happiness where you find it, and not worry about other people too much. My experience tells me that we get no more than two or three such chances in a life time, and if we let them go, we regret it for the rest of our lives." },
-                                {
-                                    title: "This is a subsection.",
-                                    content: [
-                                        { content: "I think you still love me, but we can’t escape the fact that I’m not enough for you. I knew this was going to happen. So I’m not blaming you for falling in love with another woman. I’m not angry, either. I should be, but I’m not. I just feel pain. A lot of pain. I thought I could imagine how much this would hurt, but I was wrong." }
-                                    ],
-                                    degree: 2
-                                }
-                            ],
-                            degree: 1
-                        },
-                        {
-                            title: "This is another section",
-                            content: [
-                                { content: "No truth can cure the sorrow we feel from losing a loved one. No truth, no sincerity, no strength, no kindness can cure that sorrow. All we can do is see it through to the end and learn something from it, but what we learn will be no help in facing the next sorrow that comes to us without warning." }
-                            ],
-                            degree: 1
-                        }
-                    ]}
-                    degree={0}
-                /> */}
             </Grid>
         </Grid>
     )
